0351673ca6f9a15952ff198990ed59069c03d30f
Hardikag17 <54525688+Hardikag17@users.noreply.github.com>
2021-06-20 00:07:24 +0530

    #2

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ window.onload = function() {
 
     var ctx = canvas.getContext("2d");
     var img = document.getElementById("photo");
+    ctx.font = "40px Arial";
+    var i=0;
     //ctx.drawImage(img, positionX, postionY,resizeX,resizeY);
     var positionX = ctx.canvas.offsetLeft,positionY = ctx.canvas.offsetTop;
-
+    var positionTextX = ctx.canvas.offsetLeft,positionTextY = ctx.canvas.offsetTop;
     console.log(positionX,positionY);
     
     ctx.drawImage(img,positionX,positionY);
-
+    ctx.fillText("Give My Certificate", positionTextX,positionTextY);
 
     MoveButton.onclick = function Move(e){
         
@@ -28,12 +30,54 @@ window.onload = function() {
 
                 console.log(positionX,positionY);
                 ctx.drawImage(img,positionX,positionY);
+                ctx.fillText("Give My Certificate", positionTextX,positionTextY);
             });
             
             
         });
     };
 
+    MoveText.onclick = function MoveText(e){
+        
+        console.log("Move Text clicked");
+        canvas.addEventListener("mousemove",function(){
+            
+            ctx.canvas.addEventListener("mousemove",function(e){
+                ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+                positionTextX = e.clientX;
+                positionTextY = e.clientY;
+
+                console.log(positionTextX,positionTextY);
+                ctx.drawImage(img,positionX,positionY);
+                ctx.fillText("Give My Certificate", positionTextX,positionTextY);
+            });
+            
+            
+        });
+    };
+
+    ResizeText.onclick = function ResizeText(e){
+        
+        console.log("Resize Text clicked");
+        canvas.addEventListener("mouseover",function(){
+            
+            ctx.canvas.addEventListener("mousemove",function(e){
+                ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+              
+                ctx.drawImage(img,positionX,positionY);
+                
+                ctx.font = `${i++}px Arial`;
+                ctx.fillText("Give My Certificate", positionTextX,positionTextY);
+                
+
+                
+
+            });
+            
+            
+        });
+    };
+    
     ResizeButton.onclick =function Resize(e){
         console.log("Resize button clicked");
         canvas.addEventListener("mousemove",function(){
@@ -44,6 +88,7 @@ window.onload = function() {
                 resizeY=e.clientY;
                 console.log(resizeX,resizeY);
                 ctx.drawImage(img,positionX,positionY,resizeX,resizeY);
+                ctx.fillText("Give My Certificate", positionTextX,positionTextY);
             });
             
         });
